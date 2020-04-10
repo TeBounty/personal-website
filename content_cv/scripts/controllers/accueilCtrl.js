53e0a18b27e2a2a76daa89f6cc128984a7382579
Gardenat <chloegardenat@live.fr>
2020-04-10 15:36:26 +0200

    Mise en page page d'accueil du cv et responsive.

--- content_cv/scripts/controllers/accueilCtrl.js
+++ content_cv/scripts/controllers/accueilCtrl.js
@@ -2,7 +2,7 @@
 
 angular.module('CvShow').controller("accueilCtrl", function ($scope) {
     $scope.nomPrenom = "Chloé GARDENAT";
-    $scope.metier = "Développeuse Web, Logiciel";
+    $scope.metier = "Développeuse Web/Logiciel";
     $scope.mail = "chloegardenat@live.fr";
     $scope.numero = "06 23 37 85 46";
     $scope.sitePro = "Linkedin";
