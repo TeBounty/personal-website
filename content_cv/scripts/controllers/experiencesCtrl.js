15228a62ce3d58f2bd783e6e8f12ca3003e9e640
Gardenat <chloegardenat@live.fr>
2020-04-17 14:00:38 +0200

    Onglet accueil et expériences mise en place terminer.

--- content_cv/scripts/controllers/experiencesCtrl.js
+++ content_cv/scripts/controllers/experiencesCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('CvShow').controller("experiencesCtrl", function ($scope, $route) {
+angular.module('CvShow').controller("experiencesCtrl", function($scope, $route) {
     $scope.$route = $route;
     $scope.title = "Expériences";
     $scope.message = "Voici mes expériences";
@@ -8,17 +8,18 @@ angular.module('CvShow').controller("experiencesCtrl", function ($scope, $route)
     var elements = document.querySelectorAll('#contentExp div div');
 
     for (const element of elements) {
-        element.addEventListener("click", function () {
+        element.addEventListener("click", function() {
 
             var positions = recupererPositionBouton(element);
 
             // Placer la div d'info en fonction de la position de la div cliquer
             var placementDivInfo = document.querySelector("#plusInfo");
             placementDivInfo.style.left = positions.clientX + 'px';
-            placementDivInfo.style.top = positions.clientY + 'px';
+            placementDivInfo.style.top = (positions.clientY + 100) + 'px';
             placementDivInfo.style.display = "block";
 
             // element.classList.add("activeItemE");
+
         });
     }
 
@@ -29,19 +30,46 @@ function recupererPositionBouton(a) {
     var b = a.getBoundingClientRect();
     return {
         clientX: a.offsetLeft,
-        clientY: a.offsetTop,
-        viewportX: (b.x || b.left),
-        viewportY: (b.y || b.top)
+        clientY: a.offsetTop
     }
 }
 
-// Mettre à jour le texte de la dic information en fonction du bouton cliquer
-function changeContenuDivInfo() {
-    
+// Mettre à jour le texte de la div information en fonction du bouton cliquer
+function changeContenuDivInfo(id) {
+    var information = "";
+    var divInfo = document.querySelector("#plusInfo div");
+
+    switch (id) {
+        case 'premierExp':
+            information = "<p>" + "Bonjour" +"<p/>"
+            divInfo.innerHTML = information;
+            break;
+        case 'deuxiemeExp':
+            information = 'Pom';
+            divInfo.innerHTML = information;
+            break;
+        case 'troisiemeExp':
+            information = 'Prout';
+            divInfo.innerHTML = information;
+            break;
+        case 'quatriemeExp':
+            information = 'Texte';
+            divInfo.innerHTML = information;
+            break;
+        case 'cinquiemeExp':
+            information = 'Luciole';
+            divInfo.innerHTML = information;
+            break;
+        case 'sixiemeExp':
+            information = 'Tomate';
+            divInfo.innerHTML = information;
+            break;
+    }
 }
 
 // Ferme la div si on clique sur la croix
 function closeDiv() {
     document.querySelector("#plusInfo").style.display = "none";
+
     // element.classList.remove("activeItemE");
 }
