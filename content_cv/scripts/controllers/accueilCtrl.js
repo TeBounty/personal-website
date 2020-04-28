fe46eb41a107fb46050fe2aa1262cb476647cfbc
Gardenat <chloegardenat@live.fr>
2020-04-28 19:21:52 +0200

    Avancer sur tout les onglets.

--- content_cv/scripts/controllers/accueilCtrl.js
+++ content_cv/scripts/controllers/accueilCtrl.js
@@ -5,6 +5,10 @@ angular.module('CvShow').controller("accueilCtrl", function ($scope) {
     $scope.metier = "Développeuse Web/Logiciel";
     $scope.mail = "chloegardenat@live.fr";
     $scope.numero = "06 23 37 85 46";
-    $scope.sitePro = "Linkedin";
-    $scope.siteProjet = "Git";
+
+    $scope.contentRechercher = "Je suis à la recherche d'une entreprise à l'écoute, qui souhaite m'accompagner dans mon évolution. Afin que je lui en apporte davantage que ce qu'il m'apporte.";
+    $scope.contentAimer = "De nature curieuse, j'aime toucher à tout. J'ai tout de même des loisirs particulier auquel je m'attache plus que d'autres.";
+    $scope.contentSavoir = "Je ne m'intéresse pas à un langage en particulier, j'essaye d'augmenter ma capacité à programmer. A savoir m'adapter à tout.";
+    $scope.contentEtre = "Très organisée et minutieuse c'est des qualités que j'ai gardé de mon ancien métier.";
+
 });
--- content_cv/scripts/controllers/experiencesCtrl.js
+++ content_cv/scripts/controllers/experiencesCtrl.js
@@ -15,7 +15,7 @@ angular.module('CvShow').controller("experiencesCtrl", function($scope, $route) 
             // Placer la div d'info en fonction de la position de la div cliquer
             var placementDivInfo = document.querySelector("#plusInfo");
             placementDivInfo.style.left = positions.clientX + 'px';
-            placementDivInfo.style.top = (positions.clientY + 100) + 'px';
+            placementDivInfo.style.top = (positions.clientY + 150) + 'px';
             placementDivInfo.style.display = "block";
 
             // element.classList.add("activeItemE");
@@ -41,27 +41,51 @@ function changeContenuDivInfo(id) {
 
     switch (id) {
         case 'premierExp':
-            information = "<p>" + "Bonjour" +"<p/>"
+            information = "<span>" + "Conception d'une nouvelle fonctionnalité dans le portail interne :" + "</span>" +
+            "<ul>" + 
+            "<li>Développement du front-end (html, css, javascript)</li>" + 
+            "<li>Développement du back-end (C#, base de données, procédure stockée)</li>" +
+            "</ul>"
             divInfo.innerHTML = information;
             break;
         case 'deuxiemeExp':
-            information = 'Pom';
+            information = "<span>" + "Conception d'une nouvelle fonctionnalité dans le portail interne :" + "</span>" +
+            "<ul>" + 
+            "<li>Développement du front-end (html, css, javascript)</li>" + 
+            "<li>Développement du back-end (C#, base de données, procédure stockée)</li>" +
+            "</ul>"
             divInfo.innerHTML = information;
             break;
         case 'troisiemeExp':
-            information = 'Prout';
+            information = "<span>" + "Conception d'une nouvelle fonctionnalité dans le portail interne :" + "</span>" +
+            "<ul>" + 
+            "<li>Développement du front-end (html, css, javascript)</li>" + 
+            "<li>Développement du back-end (C#, base de données, procédure stockée)</li>" +
+            "</ul>"
             divInfo.innerHTML = information;
             break;
         case 'quatriemeExp':
-            information = 'Texte';
+            information = "<span>" + "Conception d'une nouvelle fonctionnalité dans le portail interne :" + "</span>" +
+            "<ul>" + 
+            "<li>Développement du front-end (html, css, javascript)</li>" + 
+            "<li>Développement du back-end (C#, base de données, procédure stockée)</li>" +
+            "</ul>"
             divInfo.innerHTML = information;
             break;
         case 'cinquiemeExp':
-            information = 'Luciole';
+            information = "<span>" + "Conception d'une nouvelle fonctionnalité dans le portail interne :" + "</span>" +
+            "<ul>" + 
+            "<li>Développement du front-end (html, css, javascript)</li>" + 
+            "<li>Développement du back-end (C#, base de données, procédure stockée)</li>" +
+            "</ul>"
             divInfo.innerHTML = information;
             break;
         case 'sixiemeExp':
-            information = 'Tomate';
+            information = "<span>" + "Conception d'une nouvelle fonctionnalité dans le portail interne :" + "</span>" +
+            "<ul>" + 
+            "<li>Développement du front-end (html, css, javascript)</li>" + 
+            "<li>Développement du back-end (C#, base de données, procédure stockée)</li>" +
+            "</ul>"
             divInfo.innerHTML = information;
             break;
     }
