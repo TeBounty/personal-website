b4d658dba90e59ce5afa73275b8007136757880d
Gardenat <chloegardenat@live.fr>
2020-05-04 15:20:37 +0200

    Mise en page et contenu de la page d'accueil du cv.

--- content_cv/scripts/controllers/accueilCtrl.js
+++ content_cv/scripts/controllers/accueilCtrl.js
@@ -6,10 +6,10 @@ angular.module('CvShow').controller("accueilCtrl", function ($scope) {
     $scope.mail = "chloegardenat@live.fr";
     $scope.numero = "06 23 37 85 46";
 
-    $scope.contentRechercher = "Je suis à la recherche d'une entreprise à l'écoute, qui souhaite m'accompagner dans mon évolution. Afin que je lui en apporte davantage que ce qu'il m'apporte.";
-    $scope.contentAimer = "De nature curieuse, j'aime toucher à tout. J'ai tout de même des loisirs particulier auquel je m'attache plus que d'autres.";
-    $scope.contentSavoir = "Je ne m'intéresse pas à un langage en particulier, j'essaye d'augmenter ma capacité à programmer. A savoir m'adapter à tout.";
-    $scope.contentEtre = "Très organisée et minutieuse c'est des qualités que j'ai gardé de mon ancien métier.";
+    $scope.contentRechercher = "Je suis à la recherche d'un poste de développeuse junior au sein d'une entreprise qui pourra m'accompagner dans mon évolution.";
+    $scope.contentAimer = "De nature curieuse, j'aime aller à la rencontre de nouvelles connaissances. Mes loisirs sont le dessin et les jeux vidéos, qui m'ont initiés à l'univers de l'informatique depuis mon adolescence.";
+    $scope.contentSavoir = "Durant mon parcours je me suis intérressée à divers langage de programmation. Mon objectif étant de pouvoir être polyvalente et de m'adapter aux différentes missions qui pourront m'être confiées.";
+    $scope.contentEtre = "Mon parcours professionnel m'a permis de développer des qualités telles que l'organisation, le travail d'équipe et la minutie. ";
 
     // // Récupérer les div concerner par le survol de la souris
     // var elements = document.querySelectorAll("#miseEnBouche div div a h3");
